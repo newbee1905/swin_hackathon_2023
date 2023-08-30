95ee0ff8fa4b596b922281191b334753b974484c
newbee1905 <beenewminh@outlook.com>
2023-08-31 01:31:01 +1000

    fix: docker currently ignore env

--- backend/settings.py
+++
@@ -1,9 +0,0 @@
-from os import environ 
-
-SECRET_KEY = environ.get('SECRET_KEY')
-API_KEY = environ.get('API_KEY')
-
-FIREBASE_API_KEY = environ.get('FIREBASE_API_KEY')
-FIREBASE_PROJECT_ID = environ.get('FIREBASE_PROJECT_ID')
-FIREBASE_AUTH_SIGN_IN_OPTIONS = 'google'
-SECRET_KEY = environ.get('SECRET_KEY')
