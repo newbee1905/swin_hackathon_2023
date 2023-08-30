7e60c031247350e87dbc938cd597718275dec7d6
newbee1905 <beenewminh@outlook.com>
2023-08-31 03:50:43 +1000

    feat: sending image as file instead of base64

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,7 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
-from flask import Flask, request, redirect, jsonify
+from flask import Flask, request, redirect, jsonify, render_template
 from flask_firebase import FirebaseAuth
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
 import base64
@@ -68,53 +68,48 @@ def authentication_required():
 @app.route("/")
 @login_required
 def index():
-    return f"<p>Hello, {current_user.name}!</p><img src='{current_user.photo_url}' />"
+    return render_template('index.html', name=current_user.name, photo_url=current_user.photo_url)
 
 
 @app.route("/run", methods=['POST'])
 @login_required
 def run() -> str:
-    if request.get_json() is not None:
-        data = request.get_json()
+    data = request.form
 
-        err = ""
+    err = ""
 
-        image_arr = np.array([])
+    image_arr = np.array([])
 
-        image = data.get('image')
-        if image is None:
-            err += "Image is missing\n"
-        else:
-            base64_string = image.split(',')[1]
-            image_bytes = base64.b64decode(base64_string)
-            _image = Image.open(io.BytesIO(image_bytes))
-            image_arr = np.array(_image)
-
-        image_mode = data.get('image_mode')
-        if image_mode is None:
-            err += "Image Mode is missing\n"
-
-        prompt = data.get('prompt')
-        if prompt is None:
-            prompt = ""
-
-        images_number = data.get('images_number')
-        if images_number is None:
-            err += "Images Number is missing\n"
-
-        alignment_strength = data.get('alignment_strength')
-        if alignment_strength is None:
-            err += "Alignment Strength is missing\n"
-
-        tile_refinement = data.get('tile_refinement')
-        if tile_refinement is None:
-            tile_refinement = False
-
-        print (image_arr, image_mode, prompt, images_number, alignment_strength, tile_refinement)
-
-        if len(err) == 0:
-            return jsonify({'test': 'hello'}), 200
-        else:
-            return jsonify({'err': err}), 400
+    image = request.files.get('image')
+    if image is None:
+        err += "Image is missing\n"
     else:
-        return jsonify({'err': 'please send request as json'})
+        image_stream = Image.open(image.stream)
+        image_arr = np.array(image_stream)
+
+    image_mode = data.get('image_mode')
+    if image_mode is None:
+        err += "Image Mode is missing\n"
+
+    prompt = data.get('prompt')
+    if prompt is None:
+        prompt = ""
+
+    images_number = data.get('images_number')
+    if images_number is None:
+        err += "Images Number is missing\n"
+
+    alignment_strength = data.get('alignment_strength')
+    if alignment_strength is None:
+        err += "Alignment Strength is missing\n"
+
+    tile_refinement = data.get('tile_refinement')
+    if tile_refinement is None:
+        tile_refinement = False
+
+    print(image_arr, image_mode, prompt, images_number, alignment_strength, tile_refinement)
+
+    if len(err) == 0:
+        return jsonify({'test': 'hello'}), 200
+    else:
+        return jsonify({'err': err}), 400
